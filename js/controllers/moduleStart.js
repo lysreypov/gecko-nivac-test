e6f0349878b5043cd5dc7e3172961d93bcbc5a39
lysreypov <ly.sreypov19@kit.edu.kh>
2024-01-15 22:05:22 +0700

    fixing popup

--- js/controllers/moduleStart.js
+++ js/controllers/moduleStart.js
@@ -59,7 +59,7 @@ var moduleStart = {
         if (GetURLParameter("_goto")) {
           _goto(GetURLParameter("_goto"));
         } else {
-          _goto("result-page");
+          _goto("select-lang-page");
         }
       }
     );
--- js/controllers/popup.js
+++ js/controllers/popup.js
@@ -1,75 +1,13 @@
-var popup = {
-  init: function (elem) {
-    this.guide(elem);
-  },
-
-  close: function (elem) {
-    gsap.fromTo(
-      $(".group-popup#" + elem),
-      0.5,
-      {
-        scale: 1,
-        alpha: 1,
-        display: "block",
-        ease: Power4.easeOut,
-      },
-      {
-        scale: 0,
-        alpha: 0,
-        display: "none",
-        ease: Power4.easeOut,
-        delay: 0.1,
-      }
-    );
-  },
-
-  open: function (elem) {
-    if (!$("#" + elem).children().length) {
-      return gsap.fromTo(
-        $(".group-popup#" + elem),
-        1,
-        {
-          scale: 0,
-          alpha: 1,
-          display: "none",
-          ease: Power4.easeOut,
-        },
-        {
-          scale: 1,
-          alpha: 1,
-          display: "block",
-          ease: Power4.easeOut,
-          delay: 0.1,
-        }
-      );
-    } else {
-      gsap.fromTo(
-        $(".group-popup#" + elem),
-        1,
-        {
-          scale: 0,
-          alpha: 1,
-          display: "none",
-          ease: Power4.easeOut,
-        },
-        {
-          scale: 1,
-          alpha: 1,
-          display: "block",
-          ease: Power4.easeOut,
-          delay: 0.1,
-        }
-      );
-    }
-  },
-};
-
 $("#tip-btn").on("click", function () {
-  popup.open("popup");
+  $("#popup").show();
   $(this).fadeOut(animationDuration);
+  setTimeout(function () {
+    $(".desc").fadeIn(animationDuration);
+  }, 400);
 });
 
 $("#close-btn").on("click", function () {
-  popup.close("popup");
+  $(".desc").fadeOut(animationDuration);
+  $("#popup").hide();
   $("#tip-btn").fadeIn(animationDuration);
 });
